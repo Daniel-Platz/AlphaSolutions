fc114319efb89816f2bf711abd5aa6be9197c3bc
Daniel-Platz <dani0005@stud.kea.dk>
2025-04-29 18:38:35 +0200

    Added findAllProjects and findProjectByEmployeeId to ProjectRepository

--- src/main/java/org/example/alphasolutions/repository/ProjectRepository.java
+++ src/main/java/org/example/alphasolutions/repository/ProjectRepository.java
@@ -1,7 +1,11 @@
 package org.example.alphasolutions.repository;
 
+import org.example.alphasolutions.model.Project;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ProjectRepository {
@@ -10,5 +14,17 @@ public class ProjectRepository {
 
     public ProjectRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Project> findAllProjects() {
+        String sql = "SELECT * FROM projects";
+        return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Project.class));
+    }
+
+    public List<Project> findProjectsByEmployeeId(Integer employeeId) {
+        String sql = "SELECT projects.* FROM projects " +
+                "JOIN project_employees ON projects.project_id = project_employees.project_id " +
+                "WHERE project_employees.employee_id = ?";
+        return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Project.class), employeeId);
     }
 }
