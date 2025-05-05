b6591fde7deefdded9a4c3c51ad557d2ee43804b
Daniel-Platz <dani0005@stud.kea.dk>
2025-05-05 08:27:13 +0200

    Added testFindAllProjectCheckAttributes and @DirtiesContext to run multiple tests at once

--- src/test/java/org/example/alphasolutions/repository/ProjectRepositoryTest.java
+++ src/test/java/org/example/alphasolutions/repository/ProjectRepositoryTest.java
@@ -4,7 +4,9 @@ import org.example.alphasolutions.model.Project;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +16,7 @@ import java.util.List;
 
 @SpringBootTest
 @Sql(scripts = "classpath:h2init.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ProjectRepositoryTest {
 
     @Autowired
@@ -27,6 +30,18 @@ class ProjectRepositoryTest {
         assertEquals(3,projects.size(), "Should return the exact number of projects in the database");
     }
 
+    @Test
+    public void testFindAllProjectCheckAttributes() throws  SQLException {
 
+        List<Project> projects = projectRepository.findAllProjects();
+
+        assertNotNull(projects);
+        assertFalse(projects.isEmpty());
+
+        Project firstProject = projects.getFirst();
+        assertEquals(1,firstProject.getProjectId());
+        assertEquals("ERP System", firstProject.getProjectName());
+
+    }
 
 }
