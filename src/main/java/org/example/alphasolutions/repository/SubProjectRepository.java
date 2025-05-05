1b7730514995088cbeecb1a3e2f5a1aa5cb1ea69
Daniel-Platz <dani0005@stud.kea.dk>
2025-05-05 07:48:14 +0200

    Prepared SubProjectRepository for CRUD operations

--- src/main/java/org/example/alphasolutions/repository/SubProjectRepository.java
+++ src/main/java/org/example/alphasolutions/repository/SubProjectRepository.java
@@ -1,4 +1,46 @@
 package org.example.alphasolutions.repository;
 
+import org.example.alphasolutions.enums.ProjectStatus;
+import org.example.alphasolutions.model.Task;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SubProjectRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public SubProjectRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void addTask(Task newTask) {
+    }
+
+    public void removeTask(int taskId) {
+
+    }
+
+    public void getTask(int taskId) {
+
+    }
+
+    public void calculateSubProjectTotalHours() {
+
+    }
+
+    public void calculateSubProjectProgress() {
+
+    }
+
+    public void getSubProjectDeadline() {
+
+    }
+
+    public void updateSubProjectStatus(ProjectStatus subProjectStatus) {
+
+    }
+
+    public void editSubProject() {
+
+    }
 }
