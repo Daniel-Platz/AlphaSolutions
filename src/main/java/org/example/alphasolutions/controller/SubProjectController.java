311d62cfc5100f4d8f520488c06cac12e6ae3dc9
Daniel-Platz <dani0005@stud.kea.dk>
2025-05-06 20:22:06 +0200

    Setup SubProjectController and added showTasks method

--- src/main/java/org/example/alphasolutions/controller/SubProjectController.java
+++ src/main/java/org/example/alphasolutions/controller/SubProjectController.java
@@ -1,4 +1,43 @@
 package org.example.alphasolutions.controller;
 
+import jakarta.servlet.http.HttpSession;
+import org.example.alphasolutions.model.Task;
+import org.example.alphasolutions.service.SubProjectService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
 public class SubProjectController {
+
+    private final SubProjectService subProjectService;
+
+    public SubProjectController(SubProjectService subProjectService) {
+        this.subProjectService = subProjectService;
+    }
+
+    private boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("employee") != null;
+    }
+
+    @GetMapping
+    public String showTasks (@PathVariable int subProjectId, Model model, HttpSession session) {
+        if (!isLoggedIn(session)) {
+            return "redirect:/login";
+        }
+
+        List<Task> tasks = subProjectService.findTasksBySubProjectId(subProjectId);
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("subProjectId", subProjectId);
+
+        String role = (String) session.getAttribute("role");
+        model.addAttribute("role", role);
+
+        return "subProjectTasks";
+
+    }
+
 }
