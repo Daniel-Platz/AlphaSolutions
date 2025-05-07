9d14a6fe4e7f29e6a66502b9b03377c53ea41f42
Daniel-Platz <dani0005@stud.kea.dk>
2025-05-07 15:51:52 +0200

    Added addNewSubProject which handles possible null pointer exception with custom exception CreationException

--- src/main/java/org/example/alphasolutions/repository/SubProjectRepository.java
+++ src/main/java/org/example/alphasolutions/repository/SubProjectRepository.java
@@ -1,11 +1,19 @@
 package org.example.alphasolutions.repository;
 
 import org.example.alphasolutions.enums.ProjectStatus;
+import org.example.alphasolutions.exception.CreationException;
+import org.example.alphasolutions.model.Project;
+import org.example.alphasolutions.model.SubProject;
 import org.example.alphasolutions.model.Task;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -25,6 +33,31 @@ public class SubProjectRepository {
         String sql = "SELECT SUM(TASK_ESTIMATED_HOURS) FROM TASK WHERE SUB_PROJECT_ID = ?";
         Integer result = jdbcTemplate.queryForObject(sql, Integer.class, subProjectId);
         return result != null ? result : 0;
+    }
+
+    public int addNewSubProject(SubProject newSubProject) {
+        String sql = "INSERT INTO sub_project (sub_project_name, sub_project_description, sub_project_start_date, sub_project_end_date, sub_project_estimated_hours, sub_project_status)" +
+                " VALUES (?,?,?,?,?,?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, newSubProject.getSubProjectName());
+            ps.setString(2, newSubProject.getSubProjectDescription());
+            ps.setDate(3, Date.valueOf(newSubProject.getSubProjectStartDate()));
+            ps.setDate(4, Date.valueOf(newSubProject.getSubProjectEndDate()));
+            ps.setInt(5, newSubProject.getSubProjectEstimatedHours());
+            ps.setString(6, newSubProject.getSubProjectStatus().name());
+            return ps;
+        }, keyHolder);
+
+        Number key = keyHolder.getKey();
+        if (key==null) {
+            throw new CreationException();
+        }
+
+        return key.intValue();
     }
 
     public void calculateSubProjectProgress() {
