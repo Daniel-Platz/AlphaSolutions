894b6d2d4cada1468e3d35a68cfac456a6c1da2d
nial0003 <nial0003@stud.kea.dk>
2025-05-13 21:34:10 +0200

    Updated the findAllProjects service method by adding ProjectStatus to the method parameter

--- src/main/java/org/example/alphasolutions/service/ProjectService.java
+++ src/main/java/org/example/alphasolutions/service/ProjectService.java
@@ -1,5 +1,6 @@
 package org.example.alphasolutions.service;
 
+import org.example.alphasolutions.enums.ProjectStatus;
 import org.example.alphasolutions.model.Employee;
 import org.example.alphasolutions.model.Project;
 import org.example.alphasolutions.model.SubProject;
@@ -17,8 +18,8 @@ public class ProjectService {
         this.projectRepository = projectRepository;
     }
 
-    public List<Project> findAllProjects() {
-        return projectRepository.findAllProjects();
+    public List<Project> findAllProjects(ProjectStatus projectStatus) {
+        return projectRepository.findAllProjects(projectStatus);
     }
 
     public List<Project> findProjectsByEmployeeId(int employeeId) {
